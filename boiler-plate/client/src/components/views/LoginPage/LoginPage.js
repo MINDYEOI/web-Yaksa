bf438ef89287de88485afe0c45e11d8fc9c6a751
MINDYEOI <mindyeoi@khu.ac.kr>
2021-06-05 03:26:28 +0900

    [fix] Fix login page error

--- boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div style={{justifyContent:'center', alignItems: 'center', display:'flex', width:'100%'}}>
-            <form style>
+        <div style={{
+            justifyContent:'center', alignItems: 'center', display:'flex', width:'100%'
+        }}>
+            <form>
                 <label>Email</label>
                 <input type="email" value onChange />
                 <label>Password</label>
                 <input type="password" value onChange />
-
+                <br/>
                 <button>
                     Login
                 </button>

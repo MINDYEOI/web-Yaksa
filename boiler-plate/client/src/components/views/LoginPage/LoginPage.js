37a759c370e1897bdf3e95124998dcf678744842
MINDYEOI <mindyeoi@khu.ac.kr>
2021-06-05 14:00:37 +0900

    [fix] Fix login Page error (Could not open response.js in the editor.)

--- boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { response } from 'express';
+//import { response } from 'express';
 import React from 'react'
 import {useState} from 'react'
 import {useDispatch} from 'react-redux';

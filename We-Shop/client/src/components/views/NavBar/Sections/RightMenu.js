19bb0abb827902a8d0c1c19f29993ce331fb170a
MINDYEOI <mindyeoi@khu.ac.kr>
2021-06-07 03:09:54 +0900

    [feat] Implement basic register page

--- We-Shop/client/src/components/views/NavBar/Sections/RightMenu.js
+++ We-Shop/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -36,6 +36,9 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
+        <Menu.Item key="upload">
+          <a href="/upload">업로드</a>
+        </Menu.Item>
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>로그아웃</a>
         </Menu.Item>
